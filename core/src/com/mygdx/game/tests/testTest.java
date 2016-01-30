fef49d049458d853d575da8a5b09e27bdb105717
Scott Brown <sib506@york.ac.uk>
2016-01-30 19:53:01 +0000

    SetUp JUnit Testing

---
+++ core/src/com/mygdx/game/tests/testTest.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.tests;
+
+import junit.framework.TestCase;
+
+public class testTest extends TestCase {
+	
+	public void setUp(){
+		
+	}
+	
+	public void testA(){
+		assertTrue(true);
+	}
+
+}
