9eca39c79d1fc05916c3ce3b9f87796c757a9c37
jhylands <james.space.ict@gmail.com>
2016-02-16 12:05:52 +0000

    added assessment 3 comments

--- core/src/com/mygdx/game/GameState.java
+++ core/src/com/mygdx/game/GameState.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 /**
  * Represents the possible states of the game world.
+ * Added for assessment 3 (12)
  */
 public enum GameState {
     FREEROAM, PARTY_MENU, BATTLE, INTERACTION, BATTLE_DIALOGUE, OBJECTIVE_MENU, MAP
